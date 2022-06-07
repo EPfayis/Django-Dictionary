7426d22236568f5becd908cacce51d560d5c12d8
EPfayis <epmuhammedfayis@gmail.com>
2022-06-07 12:23:49 +0530

    Template added

--- myDictionary/urls.py
+++ myDictionary/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Dict, Count
+from .views import Dict, Count, DicTemplate
 
 urlpatterns = [
     path('', Dict.as_view(), name= "dict"),
     path('count/', Count.as_view(), name= "count"),
+    path('template/', DicTemplate.as_view(), name= "template"),
 ]
--- myDictionary/views.py
+++ myDictionary/views.py
@@ -6,10 +6,10 @@ from rest_framework.views import APIView
 from django.http import HttpResponse, JsonResponse
 from .models import TblSearchCount
 
+
 class Dict(APIView):
 
-    def get(self,request):
-
+    def get(self, request):
         word = request.GET["word"]
         print("request accepted.")
 
@@ -19,26 +19,50 @@ class Dict(APIView):
         objSearchCount.save()
         print("search count added")
 
-        res = requests.get("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ml&dt=t&q="+word)
+        res = requests.get("https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=ml&dt=t&q=" + word)
 
-        return HttpResponse(res.json()[0][0][0])
+        return JsonResponse({"meaning":res.json()[0][0][0]})
+
 
 class Count(APIView):
 
-    def get(self,request):
+    def get(self, request):
 
         dic = {}
 
-        qs = TblSearchCount.objects.filter(date= datetime.now().date())
+        qs = TblSearchCount.objects.filter(date=datetime.now().date())
 
         for obj in qs:
             if obj.word not in dic.keys():
                 dic[obj.word] = 1
             else:
                 dic[obj.word] = dic[obj.word] + 1
+            # Calculated the count of each word and stored in a dictionary
 
-        dic = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse= True)}
+        dic = {k: v for k, v in sorted(dic.items(), key=lambda item: item[1], reverse=True)}
+        # Sorted the dictionary by number of searches descending
 
         return JsonResponse(dic)
 
 
+class DicTemplate(APIView):
+
+    def get(self,request):
+
+        dicContent = {
+            "meaning" : "",
+            "topSearch" : {},
+        }
+
+        word = request.GET.get("word","")
+        dicContent["word"] = word
+
+        if word != "":
+            meaning = requests.get("http://127.0.0.1:8000/dict/?word=" + word).json()
+            dicContent["meaning"] = meaning["meaning"]
+
+        topSearch = requests.get("http://127.0.0.1:8000/dict/count").json()
+        dicContent["topSearch"] = topSearch
+
+
+        return render(request, "dictionary.html",dicContent)
