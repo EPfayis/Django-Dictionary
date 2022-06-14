ea76fd2766dade0ced85038ed3c7a50c2f2d496a
EPfayis <epmuhammedfayis@gmail.com>
2022-06-14 17:31:32 +0530

    Implement docker compose

--- Dictionary/settings.py
+++ Dictionary/settings.py
@@ -77,13 +77,32 @@ WSGI_APPLICATION = 'Dictionary.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
 
+DATABASES = {
+
+    'default': {
+
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+
+        'NAME': 'DbDictionary',
+
+        'USER': 'postgres',
+
+        'PASSWORD': '123123',
+
+        'HOST': 'db',
+
+        'PORT': '5432',
+
+    }
+
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
